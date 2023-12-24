08809d0a1ee49120aa8ced722371b20259c56c8e
SGO-REYNALDY\squar <renaldy@sgo.co.id>
2023-12-24 14:31:06 +0700

    update

--- src/pages/home/HomeComponents.jsx
+++ src/pages/home/HomeComponents.jsx
@@ -3,12 +3,18 @@ import Header from "../../components/header/Header";
 import Footer from "../../components/footer/Footer";
 import TopButton from "../../components/topButton/TopButton";
 import Greeting from "../../containers/greeting/Greeting";
+import Education from "../../containers/education/Education";
+import Project from "../../containers/projects/Project";
+import Contact from "../../containers/contact/Contact";
 
 function HomeComponent() {
     return(
         <div>
             <Header />
             <Greeting />
+            <Education />
+            <Project />
+            <Contact />
             <Footer />
             {/* <TopButton /> */}
         </div>
