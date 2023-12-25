589a12e4c04b4b14e14bc5f56d86c7f28ef016ab
SGO-REYNALDY\squar <renaldy@sgo.co.id>
2023-12-25 13:10:34 +0700

    update

--- src/containers/education/Education.jsx
+++ src/containers/education/Education.jsx
@@ -1,10 +1,66 @@
 import React from "react";
+import "./Education.css";
+import { Fade } from "react-reveal";
 
-function Education() {
+function Education(props) {
+    const theme = props.theme;
     return (
-        <div>
-            <h1>Education</h1>
-        </div>
+        <Fade bottom duration={2000} distance="40px">
+            <div className="education-main">
+                <div>
+                    <h1
+                        style={{ color: theme.text }}
+                        className="education-text"
+                    >
+                        Education
+                    </h1>
+                    <h2
+                        style={{ color: theme.text }}
+                        className="education-history"
+                    >
+                        Tarumanagara University
+                    </h2>
+                    <p
+                        style={{ color: theme.text }}
+                        className="education-text-p"
+                    >Experienced Web Programmer with a demonstrated history of working in the insurance industry & IT Service/Solution. Skilled in PHP, JavaScript Strong engineering professional graduated from Universitas Tarumanagara.
+                    </p>
+                    <h2
+                        style={{ color: theme.text }}
+                        className="education-history"
+                    >
+                        Tarumanagara University
+                    </h2>
+                    <p
+                        style={{ color: theme.text }}
+                        className="education-text-p"
+                    >Experienced Web Programmer with a demonstrated history of working in the insurance industry & IT Service/Solution. Skilled in PHP, JavaScript Strong engineering professional graduated from Universitas Tarumanagara.
+                    </p>
+                    <h2
+                        style={{ color: theme.text }}
+                        className="education-history"
+                    >
+                        Tarumanagara University
+                    </h2>
+                    <p
+                        style={{ color: theme.text }}
+                        className="education-text-p"
+                    >Experienced Web Programmer with a demonstrated history of working in the insurance industry & IT Service/Solution. Skilled in PHP, JavaScript Strong engineering professional graduated from Universitas Tarumanagara.
+                    </p>
+                    <h2
+                        style={{ color: theme.text }}
+                        className="education-history"
+                    >
+                        Tarumanagara University
+                    </h2>
+                    <p
+                        style={{ color: theme.text }}
+                        className="education-text-p"
+                    >Experienced Web Programmer with a demonstrated history of working in the insurance industry & IT Service/Solution. Skilled in PHP, JavaScript Strong engineering professional graduated from Universitas Tarumanagara.
+                    </p>
+                </div>
+            </div>
+        </Fade>
     );
 }
 
