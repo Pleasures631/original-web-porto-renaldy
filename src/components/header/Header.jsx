96c48cb6c8fdec8bc4af81df45e7dad3bf124ac1
SGO-REYNALDY\squar <renaldy@sgo.co.id>
2024-01-08 15:52:45 +0700

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,59 +4,53 @@ import { Fade } from "react-reveal";
 import { NavLink, Link } from "react-router-dom";
 import BackgroundGreeting from '../../../src/wave-haikei.svg';
 
-const Header = ({ theme, resultRef, resultRef2, resultRef3 }) => {    
+const Header = ({ theme, resultRef, resultRef2, resultRef3 }) => {
 
-    const handleClick = (ref) => {        
-        // Memastikan bahwa secondChildRef.current sudah terisi sebelum menggunakan
-        // if (resultRef.current) {            
-            // Memanggil fungsi pada komponen kedua untuk menggulir
-            ref.current.scrollIntoView({behavior: 'smooth'});
-        // }
+    const handleClick = (ref) => {
+        ref.current.scrollIntoView({ behavior: 'smooth' });
     };
 
     return (
-        <div>                        
-            <header className="header">
+        <div>
+            <header className="header" style={{position:"relative", zIndex:"1"}}>
                 <Fade top duration={1000} distance="20px">
                     <div>
-                        <header className="header">
-                            <input className="menu-btn" type="checkbox" id="menu-btn" />
-                            <label className="menu-icon" htmlFor="menu-btn">
-                                <span className="navicon"></span>
-                            </label>
-                            <ul className="menu" style={{ backgroundColor: theme.body }}>
-                                <li>
-                                    <NavLink
-                                        onClick={() => handleClick(resultRef)}
-                                        activeStyle={{ fontWeight: "bold" }}
-                                        style={{ color: theme.headline }}
-                                        className="header-text"
-                                    >
-                                        Home
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink
-                                        onClick={() => handleClick(resultRef2)}
-                                        activeStyle={{ fontWeight: "bold" }}
-                                        style={{ color: theme.headline }}
-                                        className="header-text"
-                                    >
-                                        Experience
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink
-                                        onClick={() => handleClick(resultRef3)}
-                                        activeStyle={{ fontWeight: "bold" }}
-                                        style={{ color: theme.headline }}
-                                        className="header-text"
-                                    >
-                                        Skills
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        </header>
+                        <input className="menu-btn" type="checkbox" id="menu-btn" />
+                        <label className="menu-icon" htmlFor="menu-btn">
+                            <span className="navicon"></span>
+                        </label>
+                        <ul className="menu">
+                            <li>
+                                <NavLink
+                                    onClick={() => handleClick(resultRef)}
+                                    activeStyle={{ fontWeight: "bold" }}
+                                    style={{ color: theme.headline }}
+                                    className="header-text"
+                                >
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    onClick={() => handleClick(resultRef2)}
+                                    activeStyle={{ fontWeight: "bold" }}
+                                    style={{ color: theme.headline }}
+                                    className="header-text"
+                                >
+                                    Experience
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink
+                                    onClick={() => handleClick(resultRef3)}
+                                    activeStyle={{ fontWeight: "bold" }}
+                                    style={{ color: theme.headline }}
+                                    className="header-text"
+                                >
+                                    Skills
+                                </NavLink>
+                            </li>
+                        </ul>
                     </div>
                 </Fade>
             </header>
